2f28a2d3954485ff8fa5cd3b0df75f312d25f486
GCaplan <gabriel.caplan@gmail.com>
2020-07-28 16:46:04 -0400

    created output folder to render files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// Dependencies==============
+// File Links
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+// Dependencies==============
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
