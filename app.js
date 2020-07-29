0a42cefe8be2e39efa7b4e70cc06d4784b38fc17
GCaplan <gabriel.caplan@gmail.com>
2020-07-28 20:25:56 -0400

    write to Output directory working. Html deliverable

--- app.js
+++ app.js
@@ -133,33 +133,25 @@ function questions() {
         });
         break;
       default:
-        const employeeData = render(employeesArray);
+        outPut();
     }
   });
 }
-questions();
 
-// function call to initialize program
+function outPut() {
+  // render function with array of employee objects
+  const employeeData = render(employeesArray);
+  fs.writeFile(outputPath, employeeData, function (err) {
+    if (err) {
+      console.log("error", err);
+    }
+    console.log("Successfully created team cards!");
+  });
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function init() {
+  console.log("***Please build your team***");
+  questions();
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-// from README
-// The different employee types should all inherit some methods and properties from a base class of Employee.
+init();
